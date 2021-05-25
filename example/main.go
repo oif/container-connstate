6e506ca241b68b499e5607dfa8c3ba2c22ded81d
Neo Zhuo <neowiz@icloud.com>
2021-05-25 14:50:41 +0800

    chore: optimize example code

--- example/main.go
+++ example/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/oif/container-connstate"
 
@@ -15,26 +17,34 @@ func panicOnError(err error) {
 }
 
 func main() {
-	containerdClient, err := containerd.New("/run/containerd/containerd.sock")
+	var containerdSocket string
+	flag.StringVar(&containerdSocket, "containerd", "/run/containerd/containerd.sock", "containerd daemon socket")
+	flag.Parse()
+
+	containerdClient, err := containerd.New(containerdSocket)
 	panicOnError(err)
-	driver, err := connstate.NewContainerdDriver(containerdClient)
+	driver, err := connstate.NewContainerdDriver(containerdClient, connstate.WithEnvCollectionFilter(func(s string) bool {
+		return strings.HasPrefix(s, "CONTAINER_")
+	}))
 	panicOnError(err)
 	tracker, err := connstate.NewTracker(driver)
 	panicOnError(err)
-	states, err := tracker.ListAllConnectionState()
+	containers, err := tracker.ListAllConnectionState()
 	panicOnError(err)
-	for _, state := range states {
+	for _, container := range containers {
 		statistics := make(map[string]uint64)
-		v4Statistics := state.IPv4.StatisticsByState()
-		v6Statistics := state.IPv6.StatisticsByState()
+		v4Statistics := container.IPv4.StatisticsByState()
+		v6Statistics := container.IPv6.StatisticsByState()
 		for stateFlag, count := range v4Statistics {
 			statistics[connstate.GetReadableState(stateFlag)] += count
 		}
 		for stateFlag, count := range v6Statistics {
 			statistics[connstate.GetReadableState(stateFlag)] += count
 		}
-		fmt.Printf("%s@%d -> %v\n", state.ID, state.PID, statistics)
-		for _, connection := range append(state.IPv4, state.IPv6...) {
+		fmt.Printf("%s@%d -> %v\n", container.ID, container.PID, statistics)
+		fmt.Printf("\t Annotations %v\n", container.Annotations)
+		fmt.Println("\t Connections")
+		for _, connection := range append(container.IPv4, container.IPv6...) {
 			fmt.Printf("\t%v %s\n", connection.Socket.ID, connstate.GetReadableState(connection.Socket.State))
 		}
 	}
